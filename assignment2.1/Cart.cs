bbc1397f83133502ae7229c475f9bcb522524bbe
Alina <a.y.kositsyna@gmail.com>
2023-03-08 17:06:26 +0700

    feat(Cart): add method CartBalancing

--- assignment2.1/Cart.cs
+++ assignment2.1/Cart.cs
@@ -7,12 +7,54 @@ using System.Threading.Tasks;
 namespace assignment2._1
 {
     internal class Cart<T>
+        where T : IFood
     {
         public List<T> Foodstuffs;
 
 
         public void CartBalancing()
-        { }
+        {
+            bool proteinExists = false;
+            bool fatsExist = false;
+            bool carbohydratesExist = false;
+
+            foreach (T foodstuff in Foodstuffs)
+            {
+                proteinExists = foodstuff.Proteins || proteinExists;
+                fatsExist = foodstuff.Fats || fatsExist;
+                carbohydratesExist = foodstuff.Carbohydrates || carbohydratesExist;
+            }
+
+            if (proteinExists)
+            {
+                Console.WriteLine("Not enough protein :(");
+            }
+
+            if (fatsExist)
+            {
+                Console.WriteLine("Not enough fats :(");
+            }
+
+            if (carbohydratesExist) 
+            {
+                Console.WriteLine("Not enough carbohydrates :(");
+            }
+
+            if (proteinExists && fatsExist && carbohydratesExist) 
+            {
+                Console.WriteLine("Cart is unbalanced. Wanna balace it? (y/n)");
+                string answer = Console.ReadLine();
+                if (answer == "y")
+                {
+                    //start balancing
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("Your car is well balanced :)");
+            }
+        }
         
     }
 }
